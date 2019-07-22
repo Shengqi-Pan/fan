9f0d241ad0155946efa12348ccc5d53b50027b91
Freedom-is-slavery <gzxxwwz@126.com>
2019-07-22 20:25:21 +0800

    test2
    test2

--- Debug/pwm.pp
+++ Debug/pwm.pp
@@ -2,16 +2,16 @@
 
 pwm.obj: ../pwm.c
 pwm.obj: ../pwm.h
-pwm.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430.h
-pwm.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430f5438a.h
-pwm.obj: C:/ti/ccsv6/ccs_base/msp430/include/in430.h
-pwm.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/intrinsics.h
-pwm.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/intrinsics_legacy_undefs.h
+pwm.obj: D:/ti/ccsv6/ccs_base/msp430/include/msp430.h
+pwm.obj: D:/ti/ccsv6/ccs_base/msp430/include/msp430f5438a.h
+pwm.obj: D:/ti/ccsv6/ccs_base/msp430/include/in430.h
+pwm.obj: D:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/intrinsics.h
+pwm.obj: D:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/intrinsics_legacy_undefs.h
 
 ../pwm.c: 
 ../pwm.h: 
-C:/ti/ccsv6/ccs_base/msp430/include/msp430.h: 
-C:/ti/ccsv6/ccs_base/msp430/include/msp430f5438a.h: 
-C:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
-C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/intrinsics.h: 
-C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/intrinsics_legacy_undefs.h: 
+D:/ti/ccsv6/ccs_base/msp430/include/msp430.h: 
+D:/ti/ccsv6/ccs_base/msp430/include/msp430f5438a.h: 
+D:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+D:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/intrinsics.h: 
+D:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/intrinsics_legacy_undefs.h: 
